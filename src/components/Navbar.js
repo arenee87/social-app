e3416f507df694ae485c54982244d5e6459e4946
Alece Kemp <alece.kemp@gmail.com>
2023-07-08 18:05:21 -0500

    navbar icon badges

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import "./navbar.css"
-import { Search } from "@mui/icons-material"
+import { Search , Person, Chat, Notifications } from "@mui/icons-material"
 
 export default function Navbar() {
     return (
@@ -15,6 +15,24 @@ export default function Navbar() {
                 </div>
 
             <div className="navbarRight"></div>
+            <div className="navbarLinks">
+                <span className="navbarLink">Homepage</span>
+                <span className="navbarLink">Timeline</span>
+            </div>
+            <div className="navbarIcons"></div>
+                <div className="navbarIconItem">
+                    <Person />
+                    <span className="navbarIconBadge">1</span>
+                </div>
+                <div className="navbarIconItem">
+                    <Chat />
+                    <span className="navbarIconBadge">2</span>
+                </div>
+                <div className="navbarIconItem">
+                    <Notifications />
+                    <span className="navbarIconBadge">7</span>
+                </div>
+
         </div>
     )
 }
