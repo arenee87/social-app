eb110776ee59f024bbc966451a6c9af634f9e3a0
Alece Kemp <alece.kemp@gmail.com>
2023-07-15 00:36:51 -0500

    organized and styled the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,31 +8,34 @@ export default function Navbar() {
                 <span className="logo">AleceSocial</span>
             </div>
 
-            <div className="navbarCenter"></div>
+            <div className="navbarCenter">
                 <div className="searchbar">
-                    <Search />
+                    <Search className="searchIcon"/>
                     <input placeholder="Search friends, posts, videos.." className="searchInput" />
                 </div>
-
-            <div className="navbarRight"></div>
-            <div className="navbarLinks">
-                <span className="navbarLink">Homepage</span>
-                <span className="navbarLink">Timeline</span>
             </div>
-            <div className="navbarIcons"></div>
-                <div className="navbarIconItem">
-                    <Person />
-                    <span className="navbarIconBadge">1</span>
+            <div className="navbarRight">
+                <div className="navbarLinks">
+                    <span className="navbarLink">Homepage</span>
+                    <span className="navbarLink">Timeline</span>
                 </div>
-                <div className="navbarIconItem">
-                    <Chat />
-                    <span className="navbarIconBadge">2</span>
+            
+                <div className="navbarIcons">
+                    <div className="navbarIconItem">
+                        <Person />
+                        <span className="navbarIconBadge">1</span>
+                    </div>
+                    <div className="navbarIconItem">
+                        <Chat />
+                        <span className="navbarIconBadge">2</span>
+                    </div>
+                    <div className="navbarIconItem">
+                        <Notifications />
+                        <span className="navbarIconBadge">7</span>
+                    </div>
                 </div>
-                <div className="navbarIconItem">
-                    <Notifications />
-                    <span className="navbarIconBadge">7</span>
-                </div>
-
+                <img src="assests/assests/person/2.person.jpeg" alt="" className="navbarImg" />
+            </div>
         </div>
     )
 }
