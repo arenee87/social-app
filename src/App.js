45bde696358b20ae16d69ba9f5797b9422e0d4c1
Alece Kemp <alece.kemp@gmail.com>
2023-09-18 10:06:22 -0500

    completed register page and added Home component to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import Login from "./pages/login/Login";
-import Profile from "./pages/profile/Profile";
+import Home from "./pages/home/Home";
+
 
 
 
 function App() {
   return(
-    <Login/>
+    <Home/>
     
   )
 }
